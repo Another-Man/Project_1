d40d19a967f5b0395a086b994fdc5fb23a490319
AnotherMan <ivan.denisyuk1998@gmail.com>
2020-10-05 22:10:17 +0300

    Personally backed all the changes to the 'Added a JS folder', but now it is named 'js'

--- js/script.js
+++ js/script.js
@@ -12,14 +12,11 @@ const personalMovieDB = {
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
       b = prompt('На сколько вы оцените его?', ''),
-      c = prompt('Какую роль играл ваш любимый актер из этого фильма?', ''),
-      d = prompt('как зовут этого актера?', ''),
-      e = prompt('Какой жанр у этого фильма?', ''),
-      f = confirm('Вы смотрели его в одиночестве, или с другом?');
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько вы оцените его?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.actors[c] = d;
-personalMovieDB.genres[0] = e;
-personalMovieDB.private = f;
+personalMovieDB.movies[c] = d;
+
 
 console.log(personalMovieDB);
