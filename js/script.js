a53587b564e00da1082846a47fd87da7daad1ef7
AnotherMan <ivan.denisyuk1998@gmail.com>
2020-10-24 02:31:11 +0300

    Automated questioning based on the lecturer's example. I thought it would've been easier, but it is not. Also added a rating based on a number of films watched

--- js/script.js
+++ js/script.js
@@ -10,13 +10,29 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько вы оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько вы оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
 
+
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    alert('Вы посмотрели довольно мало фильмов!');
+} else if (personalMovieDB.count < 30 && personalMovieDB.coumt >= 10) {
+    alert('Вы - любитель!');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы посмотрели много фильмов!');
+} else {
+    alert('Произошла ошибка!');
+}
