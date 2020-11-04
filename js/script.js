f1868fbe3cf447d7af3716ea6a9f28a702ec5583
AnotherMan <ivan.denisyuk1998@gmail.com>
2020-11-04 23:17:16 +0300

    I've rebuilt all of the code. All of the functions are now methods, imbedded into the main object. They are also now called manually in the browser, not automatically popping uo. Added a few new ones too.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,74 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many films did you watch?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many films did you watch?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    start: function () {
+        personalMovieDB.count = +prompt('How many films did you watch?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many films did you watch?', '');
+        }
+    },
+    moviesQuestions: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("What's the name of the latest one?", ''),
+                  b = prompt('How did you like it?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('Done!');
+            } else {
+                console.log('Error!');
+                i--;
+            }
+        
+        
+        }
+    },
+    genresQuestions: function () {
+            for (let i = 1; i < 2; i++) {
+                let a = prompt(`Write down your favourite movie genres devided by ", "`, '');
+
+                if (a != null && a != '') {
+                    personalMovieDB.genres = a.split(', ');
+                    console.log('Done!');
+                } else {
+                    console.log('Error!');
+                    i--;
+                }
+                personalMovieDB.genres.forEach((item, i) => {
+                    console.log(`Your favourite genre #${i + 1} - is ${item}`);
+                });
+            }
+    },
+    countAnswer: function() {
+        if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+            alert("So... you're not that into movies, huh?");
+        } else if (personalMovieDB.count < 30 && personalMovieDB.coumt >= 10) {
+            alert("You're an amateure. (but in a good way!)");
+        } else if (personalMovieDB.count >= 30) {
+            alert("You're a cinema MANIAC!!!");
+        } else {
+            alert("Something's wrong! :c");
+        }
+    },
+    privateQuestions: function () {
+        if (personalMovieDB.private) {
+            personalMovieDB.private = false;
+        } else {
+            personalMovieDB.private = true;
+        }
+    },
+    privateCheck: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }
 };
 
 
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("What's the name of the latest one?", ''),
-              b = prompt('How did you like it?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('Done!');
-        } else {
-            console.log('Error!');
-            i--;
-        }
-    
-    
-    }
-}
-
-rememberMyFilms();
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Your favourite movie genre #${i}`, '');
-    }
-}
-
-writeYourGenres();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
-        alert("So... you're not that into movies, huh?");
-    } else if (personalMovieDB.count < 30 && personalMovieDB.coumt >= 10) {
-        alert("You're an amateure. (but in a good way!)");
-    } else if (personalMovieDB.count >= 30) {
-        alert("You're a cinema MANIAC!!!");
-    } else {
-        alert("Something's wrong! :c");
-    }
-}
-
-detectPersonalLevel();
-
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.private);
